dede42dbe45eccc89bb0da6a05ba6d4e5ecbc705
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2021-06-07 13:14:25 -0700

    Added some more info text

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
+
+        // Using ArrayList instead of LinkedList because I am assuming
+        // that we will have random access more that adding and removing patients
+        ArrayList<Patient> patients = new ArrayList<>();
+
         System.out.println("Welcome to [Error: NotImplementedYet]");
         System.out.println("Please [Error: NotImplementedYet]");
+
+        Communication.listCommands();
+
+        //TODO actually do something meaningful
 
     }
 }
