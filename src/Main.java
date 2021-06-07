cf860133118cdf0670baa8928186795cf71be370
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2021-06-07 13:16:00 -0700

    moved communications to Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,28 @@ public class Main {
         System.out.println("Welcome to [Error: NotImplementedYet]");
         System.out.println("Please [Error: NotImplementedYet]");
 
-        Communication.listCommands();
+        listCommands();
+
+        String nextLine = scnr.nextLine();
+
+        handleInput(nextLine);
 
         //TODO actually do something meaningful
 
+    }
+
+
+    public static void handleInput(String input){
+
+    }
+
+    public static void listCommands(){
+        System.out.println("[0] View current patients");
+        System.out.println("[1] Search current patients");
+        System.out.println("[2] Edit patient info");
+        System.out.println("[3] Add patient");
+        System.out.println("[4] Remove patient");
+
+        System.out.println("[Q/q] Quit");
     }
 }
