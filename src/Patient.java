6e0e8f86aa622c6a5aedc4b5d6f5f9522ec62c16
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2021-06-07 13:04:40 -0700

    Added dummy patient class so that the comparator classes don't throw a fit

---
+++ src/Patient.java
@@ -0,0 +1,12 @@
+public class Patient {
+
+    public String getName(){
+        return "";
+    }
+    public int getAcuity(){
+        return 0;
+    }
+    public String getProvider(){
+        return "";
+    }
+}
