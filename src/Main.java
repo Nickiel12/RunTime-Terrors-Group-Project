d9c4bd4e0f34d6e8607599850b36e63b7bd4caa1
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2021-06-12 12:58:03 -0700

    DRY, and general UI changes

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,6 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-// TODO standardize and error catch user-inputs
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -35,18 +33,16 @@ public class Main {
         boolean doLoop = true;
         while (doLoop) {
             System.out.println("[0] View patients");
-            System.out.println("[1] Search patients");
+            System.out.println("[1] Search for patient");
             System.out.println("[2] Edit patient info");
             System.out.println("[3] Add patient");
             System.out.println("[4] Remove patient");
             System.out.println("[5] View working employees");
-            System.out.println("[-1] Return to main menu");
 
 
             System.out.println("[9] Quit");
             int commandNum = getInputNumber(scnr);
             switch (commandNum) {
-                case -1 -> {System.out.println("Returning to main menu"); return;}
                 case 0 -> viewPatients(scnr, patients);
                 case 1 -> searchAndView(scnr, patients);
                 case 2 -> editPatientInfo(scnr, patients, employees);
@@ -107,78 +103,15 @@ public class Main {
             case 3 -> patients1.sort(new PatientSortByProvider());
             default -> System.out.println("I didn't recognize that command. Returning to main menu");
         }
-
-        Patient target = listAndGetPatient(scnr, patients1);
-        if (target != null) {
-            System.out.println(target);
-            target.printProblemList();
-            System.out.println();
+        for (Patient p: patients1) {
+            System.out.println(p);
         }
         System.out.println("Press enter to continue to main menu");
         scnr.nextLine();
     }
 
     public static void searchAndView(Scanner scnr, Storage patients){
-
-        Patient[] patients1 = patients.getList().toArray(Patient[]::new);
-        Arrays.sort(patients1, new PatientSortByAcuity());
-        System.out.println("[0] Search by Name");
-        System.out.println("[1] Search by Acuity");
-        System.out.println("[2] Search by ID");
-        System.out.println("[3] Search by Provider");
-        System.out.println("[-1] Return to main menu");
-
-        int i = getInputNumber(scnr);
-        ArrayList<Patient> found = new ArrayList<>();
-
-        switch(i){
-            case -1 -> {System.out.println("Returning to main menu"); return;}
-            case 0 -> {
-                System.out.print("Enter the Name you would like to search: ");
-                String name = scnr.nextLine();
-                for (Patient p : patients1) {
-                    if (p.getName().contains(name) || (p.getFirstName() + " " + p.getLastName()).contains(name)
-                        || (p.getFirstName() + ", " + p.getLastName()).contains(name)){
-                        found.add(p);
-                    }
-                }
-            }
-            case 1 -> {
-                System.out.print("Enter the Acuity score: ");
-                int score = getInputNumber(scnr);
-                for (Patient p: patients1){
-                    if (p.getAcuity() == score){
-                        found.add(p);
-                    }
-                }
-            }
-            case 2 -> {
-                System.out.print("Enter the ID of the patient: ");
-                int id = getInputNumber(scnr);
-                for (Patient p: patients1){
-                    if (p.getIdNumber() == id){
-                        found.add(p);
-                    }
-                }
-            }
-            case 3 -> {
-                System.out.print("Enter the Provider you would like to search: ");
-                String name = scnr.nextLine();
-                for (Patient p : patients1) {
-                    if (p.getProvider().contains(name)){
-                        found.add(p);
-                    }
-                }
-            }
-        }
-        Patient target;
-        if (found.size() > 1){
-            target = listAndGetPatient(scnr, found);
-        } else if (found.size() == 1) target = found.get(0);
-        else {
-            System.out.println("No Patients were found");
-            return;
-        }
+        Patient target = findPatient(scnr, patients);
         if (target != null) {
             System.out.println(target);
             target.printProblemList();
@@ -206,12 +139,13 @@ public class Main {
 
     public static void editPatientInfo(Scanner scnr, Storage patients, ArrayList<Employee> employees){
         Patient target = findPatient(scnr, patients);
-        System.out.println(target);
         if (target == null) return;
+        System.out.println(target);
         System.out.println("What would you like to change?");
         System.out.println("[0] Acuity");
         System.out.println("[1] Provider");
         System.out.println("[2] Problems List");
+        System.out.println("[3] Name");
         System.out.println("[-1] Return to main menu");
 
         int i = getInputNumber(scnr);
@@ -227,7 +161,26 @@ public class Main {
             }
             case 1 -> {
                 Employee newProvider = listAndGetEmployee(scnr, employees);
+                if (newProvider != null)
                 target.setProvider(newProvider.getFirstName() + " " + newProvider.getLastName());
+            }
+            case 3 -> {
+                System.out.println("[0] First Name");
+                System.out.println("[1] Last Name");
+                System.out.println("[-1] Cancel");
+                int j = getInputNumber(scnr);
+                switch (j){
+                    case 0 ->  {
+                        System.out.print("Enter a new First Name: ");
+                        String newName = scnr.nextLine();
+                        target.setFirstName(newName);
+                    }
+                    case 1 -> {
+                        System.out.print("Enter a new Last Name:");
+                        String newName = scnr.nextLine();
+                        target.setLastName(newName);
+                    }
+                }
             }
         }
         System.out.println("Would like to make another change? (y/n)");
@@ -235,7 +188,6 @@ public class Main {
     }
 
     public static void editPatientProblems(Scanner scnr, Patient target){
-        LinkedList<String> currentProblems = target.getProblemList();
         System.out.println("Would you like to remove or add problem to list?");
         System.out.println("[0] Remove index");
         System.out.println("[1] Add new");
@@ -243,10 +195,11 @@ public class Main {
         int i = getInputNumber(scnr);
 
         if (i == -1){System.out.println("Returning to main menu"); return;}
+
+        LinkedList<String> currentProblems = target.getProblemList();
         if (i == 0){
             int j = 0;
-            for (String s :
-                    currentProblems) {
+            for (String s : currentProblems) {
                 System.out.printf("[%d] %s", j++, s);
             }
             i = getInputNumber(scnr);
@@ -284,31 +237,86 @@ public class Main {
         }
     }
 
-    public static Patient findPatient(Scanner scnr, Storage patients){
-        System.out.println("Enter a Patients Name: ");
-        String patName = scnr.nextLine();
+    public static ArrayList<Patient> searchPatients(Scanner scnr, Storage patients){
+        Patient[] patients1 = patients.getList().toArray(Patient[]::new);
+        Arrays.sort(patients1, new PatientSortByAcuity());
+        System.out.println("[0] Search by Name");
+        System.out.println("[1] Search by Acuity");
+        System.out.println("[2] Search by ID");
+        System.out.println("[3] Search by Provider");
+        System.out.println("[-1] Return to main menu");
+
+        int i = getInputNumber(scnr);
         ArrayList<Patient> found = new ArrayList<>();
-        for (Patient p1 : patients.getList()) {
-            if (p1.getName().contains(patName)) found.add(p1);
+
+        switch(i){
+            case -1 -> {System.out.println("Returning to main menu"); return null;}
+            case 0 -> {
+                System.out.print("Enter the Name you would like to search: ");
+                String name = scnr.nextLine();
+                for (Patient p : patients1) {
+                    if (p.getName().contains(name) || (p.getFirstName() + " " + p.getLastName()).contains(name)
+                            || (p.getFirstName() + ", " + p.getLastName()).contains(name)){
+                        found.add(p);
+                    }
+                }
+            }
+            case 1 -> {
+                System.out.print("Enter the Acuity score: ");
+                int score = getInputNumber(scnr);
+                for (Patient p: patients1){
+                    if (p.getAcuity() == score){
+                        found.add(p);
+                    }
+                }
+            }
+            case 2 -> {
+                System.out.print("Enter the ID of the patient: ");
+                int id = getInputNumber(scnr);
+                for (Patient p: patients1){
+                    if (p.getIdNumber() == id){
+                        found.add(p);
+                    }
+                }
+            }
+            case 3 -> {
+                System.out.print("Enter the Provider you would like to search: ");
+                String name = scnr.nextLine();
+                for (Patient p : patients1) {
+                    if (p.getProvider().contains(name)){
+                        found.add(p);
+                    }
+                }
+            }
         }
-        if (found.size() == 0){
-            System.out.println("No patients with that name found");
+        return found;
+    }
+
+    public static Patient findPatient(Scanner scnr, Storage patients){
+        ArrayList<Patient> found = searchPatients(scnr, patients);
+
+        if (found == null) return null;
+
+        System.out.printf("%d Patient(s) found\n", found.size());
+        Patient target;
+        if (found.size() > 1){
+            target = listAndGetPatient(scnr, found);
+        } else if (found.size() == 1) target = found.get(0);
+        else {
+            System.out.println("No Patients were found");
             return null;
-        } else if (found.size() == 1){
-            return found.get(0);
-        } else {
-            System.out.println("Multiple patients with that name were found,\nPlease select with one you want:");
-            return listAndGetPatient(scnr, found);
         }
+        return target;
     }
 
     public static Patient listAndGetPatient(Scanner scnr, ArrayList<Patient> patients){
+        System.out.println("[-1] Return to main menu");
         for (int i = 0; i < patients.size(); i++) {
             System.out.printf("[%d] %s\n", i, patients.get(i));
         }
         System.out.println("Which Patient did you mean?");
         int patNum = getInputNumber(scnr);
-        scnr.nextLine();
+        if (patNum == -1) return null;
         return patients.get(patNum);
     }
 
@@ -319,7 +327,7 @@ public class Main {
         }
         System.out.println("Which employee did you mean?");
         int patNum = getInputNumber(scnr);
-        scnr.nextLine();
+        if (patNum == -1) return null;
         return employees.get(patNum);
     }
 
