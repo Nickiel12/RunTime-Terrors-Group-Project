3b6c78dd3ab0c41ec0da98ee8e23e719f1bec8e8
3ngineer991 <83616316+3ngineer991@users.noreply.github.com>
2021-06-13 12:23:40 -0700

    no more NullPointerException
    No longer throws NullPointerException when sorting nulls

--- src/PatientSortByProvider.java
+++ src/PatientSortByProvider.java
@@ -2,6 +2,14 @@ import java.util.Comparator;
 
 public class PatientSortByProvider implements Comparator<Patient> {
     public int compare(Patient p1, Patient p2){
+        //deals with null values
+        if (p1.getProvider() == null) {
+            return 1;
+        }
+        if (p2.getProvider() == null) {
+            return -1;
+        }
+        
         int diff = p1.getProvider().compareTo(p2.getProvider());
 
         // if providers are the same, return in alphabetical order
